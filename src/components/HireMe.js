a11b587eddfc6c06d9ad6ab1a84bf0c5597aa09a
Kaoutar Laouaj <kaoutar.laouaj@ump.ac.ma>
2026-02-23 11:12:33 +0100

    changed

--- src/components/HireMe.js
+++ src/components/HireMe.js
@@ -12,7 +12,7 @@ const HireMe = () => {
               hover:dark:border-light
               md:w-12 md:h-12 md:text-[10px]
              ">
-                Contact Me </Link>
+             </Link>
            </div> 
         </div>
     )
